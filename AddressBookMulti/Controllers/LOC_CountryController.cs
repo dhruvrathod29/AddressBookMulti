8f1d43d23f0a2a8d3305265722f0e8906d41383a
Dhruv Rathod <86467492+dhruvrathod29@users.noreply.github.com>
2023-02-13 08:00:22 +0530

    'Index','LayoutPage','LOC_CountryAdd/Ediit_List'

---
+++ AddressBookMulti/Controllers/LOC_CountryController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AddressBookMulti.Controllers
+{
+    public class LOC_CountryController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View("LOC_CountryAddEdit");
+        }
+    }
+}
